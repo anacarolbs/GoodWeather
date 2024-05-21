7ca77de443e9489de9c41895686b89619b9213fd
Ana Carolina Barbosa de Souza <souzaanacarolb@gmail.com>
2024-05-21 19:49:57 -0300

    feat: final settings

---
+++ GoodWeather/Custom Cells/WeatherCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by Ana Carolina Barbosa de Souza on 21/05/24.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+}
